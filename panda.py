e36468d9c76b6207bcbc071cc779c665c972ea92
yogi1003 <40657933+yogi1003@users.noreply.github.com>
2021-04-19 15:34:09 +0700

    Add files via upload

---
+++ panda.py
@@ -0,0 +1,14 @@
+def ubah():
+	huruf_asli =    'abcdefghijklmnopqrstuvwxyz'
+	ngindek_satu = huruf_asli.index(soal_satu)
+	ngindek_dua = int(soal_dua)-1
+	selisih = ngindek_satu - ngindek_dua
+	huruf_kedua=huruf_asli[selisih:]+huruf_asli[:selisih]
+	hasil = kata.maketrans(huruf_kedua, huruf_asli)
+	return kata.translate(hasil)
+	
+while True:
+	soal_satu, soal_dua = input('masukkan soal(contoh k=2 jadi, k 2): ').split()
+	print('SOAL SAAT INI:\n', soal_satu, '=', soal_dua)
+	kata = input('masukkan kata: ')
+	print(ubah())
